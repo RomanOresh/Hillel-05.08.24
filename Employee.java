d6a0d0876c224944ffc351eb625b3fc979882bbd
Roman <romanoreshchenko@gmail.com>
2024-08-06 11:15:22 +0200

    Initial commit

---
+++ Employee.java
@@ -0,0 +1,21 @@
+public class Employee {
+    private String fullName;
+    private String rank;
+    private String email;
+    private int phoneNumber;
+    private int age;
+
+    public Employee(String fullName, String rank, String email, int phoneNumber, int age) {
+        this.fullName = fullName;
+        this.rank = rank;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+
+
+
+}
+
+
